ee5b684465a05e8da9ee5f2f8ca96f3bb0f262b9
Greg Kapp <gregkapp@gmail.com>
2011-11-26 01:07:18 -0800

    Done with static pages

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -13,7 +13,13 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+  end
+end
+  
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
     end
   end
-
 end
