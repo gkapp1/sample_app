8d1a553daa10934e810be1857314a6813b15c181
Greg Kapp <gregkapp@gmail.com>
2011-12-31 02:30:49 -0800

    Almost slit my wrists, but admin, destroy and user view and edit and destroy features integrated

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,14 @@
-#By using the symbol ':user', we can get Factory Girl to simulate the User model.
-
 Factory.define :user do |user|
 	user.name			          		"Gregory Kapp"
 	user.email					        "gregkapp@gmail.com"
 	user.password				        "foobar"
 	user.password_confirmation	"foobar"
 end	
+
+Factory.sequence :name do |n|
+  "Person #{n}"
+end
+
+Factory.sequence :email do |n|
+  "person-#{n}@example.com"
+end
